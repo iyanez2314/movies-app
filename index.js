ed6ce38e1616bb5f86c42fa700cc20240857ffda
Isaac Yanez <isaac231467@gmail.com>
2023-02-04 21:43:38 -0600

    was able to get the list of items in the db to work exactly how I wanted it to now all I need to do is make the add to list work for the headerClass and I am done

--- index.js
+++ index.js
@@ -184,16 +184,16 @@ class HeaderCard {
   }
   renderCard() {
     // Attach this created card to the page
-    document.querySelector(".header-content").remove();
+    document.querySelector("#myCard").remove();
     let html = `
-      <div id="myCard" class="d-flex justify-content-evenly m-5 p-5 w-100" data-movie="${this.id}">
+      <div id="myCard" class="remove d-flex justify-content-evenly m-5 p-5 w-100" data-movie="${this.id}">
                <div class="d-flex justify-content-center">
                   <img
                    id="movie-image"
                    src="http://image.tmdb.org/t/p/w500/${this.image}"
                  />
                 </div>
-              <div class="d-flex flex-column justify-content-center align-items-center m-5 p-5 w-50">
+              <div id="myCardHeader" class="d-flex flex-column justify-content-center align-items-center m-5 p-5 w-50">
                 <h1 id="movie-title">${this.title}</h1>
                    <h4>Ratings</h4>
                 <div class="d-flex justify-content-center flex-row m-4">
@@ -212,6 +212,73 @@ class HeaderCard {
   }
 }
 
+class MovieList {
+  constructor(data) {
+    this.data = data;
+    this.id = data.id;
+    this.title = data.title;
+    this.overview = data.overview;
+    this.image = data.image;
+    this.button = null;
+    this.renderCards();
+    this.addEventListeners();
+  }
+  renderCards() {
+    let html = `
+    <div id="myCard" class="remove d-flex justify-content-evenly m-5 p-5 w-100" data-movie="${this.id}">
+             <div class="d-flex justify-content-center">
+                <img
+                 id="movie-image"
+                 src="${this.image}"
+               />
+              </div>
+            <div id="myCardHeader" class="d-flex flex-column justify-content-center align-items-center m-5 p-5 w-50">
+              <h1 id="movie-title">${this.title}</h1>
+                 <h4>Ratings</h4>
+              <div class="d-flex justify-content-center flex-row m-4">
+                   <span  class="fa fa-star checked"></span>
+                 <span class="fa fa-star checked"></span>
+                   <span  class="fa fa-star"></span>
+                 <span  class="fa fa-star"></span>
+                 <span  class="fa fa-star"></span>
+                 </div>
+                <p id="movie-overview">${this.overview}</p>
+                <button class="removeFromList" type="button">Remove Form List</button>
+               </div>
+             </div>
+    `;
+    document
+      .querySelector("#list-of-movies")
+      .insertAdjacentHTML("afterend", html);
+  }
+  addEventListeners() {
+    let element = document.querySelector(".removeFromList");
+    this.button = element;
+    this.button.addEventListener("click", this.removeMovie.bind(this));
+  }
+  removeMovie() {
+    fetch(`${BASE_URL}/${this.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json(response))
+      .then((data) => console.log("Success"))
+      .catch((err) => console.log("OOPS error occured"));
+  }
+}
+
+(function () {
+  fetch(BASE_URL)
+    .then((response) => response.json(response))
+    .then((data) => {
+      data.forEach((movie) => {
+        const newList = new MovieList(movie);
+      });
+    });
+})();
+
 function testPost(e) {
   e.preventDefault();
   const movieTitle = movieTitleElem.value;
@@ -223,7 +290,6 @@ function testPost(e) {
     .then((res) => res.json(res))
     .then((data) => {
       const returnedData = data.results[0];
-      console.log(returnedData);
       let headerCard = new HeaderCard(returnedData);
     })
     .catch((err) => console.log(err));
